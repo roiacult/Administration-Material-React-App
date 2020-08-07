a872d0d74b7ffea93f1097b97517bf5cf0ce7651
roiacult <roiacult27@gmail.com>
2020-08-07 23:39:06 +0100

    create && implement login page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./App.css";
 import { Router } from "@reach/router";
+import Auth from "../pages/auth/Auth";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#ff6659",
+      main: "#d32f2f",
+      dark: "#9a0007",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#74ffff",
+      main: "#2fd3d3",
+      dark: "#00a1a2",
+      contrastText: "#000000",
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div className="App">
-      <Router></Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Auth path="/login" />
+      </Router>
+    </ThemeProvider>
   );
 };
 
