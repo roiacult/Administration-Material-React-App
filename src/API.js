85a075c83e8e8101ea83282d26b8af50427a6846
roiacult <roiacult27@gmail.com>
2020-08-11 12:16:01 +0100

    fix headers prblm & add Axios interceptor

--- src/API.js
+++ src/API.js
@@ -1,15 +1,15 @@
 import Axios from "axios";
 
-const headers = {
-  "Content-Type": "application/json",
-};
-
-if (localStorage.token) headers.Authorization = `token ${localStorage.token}`;
-
 const API = Axios.create({
   baseURL: "https://israfli.herokuapp.com/api/",
   timeout: 10000,
-  headers,
+});
+
+API.interceptors.request.use((config) => {
+  config.headers["Content-Type"] = "application/json";
+  if (localStorage.token)
+    config.headers.Authorization = `token ${localStorage.token}`;
+  return config;
 });
 
 export default API;
